753612bed2ff2c65b2a0e0bfa7ae91da524e80ff
tannt <tannt@vietis.com.vn>
2022-08-02 13:26:49 +0700

    vue

--- src/main.js
+++ src/main.js
@@ -7,7 +7,7 @@ import "./lib/tempusdominus/css/tempusdominus-bootstrap-4.min.css";
 import "./assets/handlecss.js";
 // import "./lib/wow/wow.min.js";
 // import "./lib/easing/easing.min.js";
-import "./lib/waypoints/waypoints.min.js";
+// import "./lib/waypoints/waypoints.min.js";
 import "./lib/counterup/counterup.min.js";
 import "./lib/owlcarousel/owl.carousel.min.js";
 // import "./lib/tempusdominus/js/moment.min.js";
