3cd173bf743cb6088d6b0cb0a5e9eea40b83c48d
tannt <tannt@vietis.com.vn>
2022-08-02 09:03:08 +0700

    vue

--- src/main.js
+++ src/main.js
@@ -1,6 +1,15 @@
 import { createApp } from "vue";
 import "./assets/style.css";
 import "./assets/handlecss";
+import "./lib/wow/wow.min.js";
+import "./lib/easing/easing.min.js";
+import "./lib/waypoints/waypoints.min.js";
+import "./lib/counterup/counterup.min.js";
+import "./lib/owlcarousel/owl.carousel.min.js";
+import "./lib/tempusdominus/js/moment.min.js";
+import "./lib/tempusdominus/js/moment-timezone.min.js";
+import "./lib/tempusdominus/js/tempusdominus-bootstrap-4.min.js";
+import "./js/dselect.js";
 import App from "./App.vue";
 import router from "./router";
 
